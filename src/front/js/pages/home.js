901c5a1469d794c7f60c7ea0b591a29976e6ae8d
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-02-23 13:58:20 +0000

    Update existing components/pages

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,22 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  if (store.token && store.token != "" && store.token != undefined) {
+    return (
+      <div className="text-center mt-5">
+        <h1>Welcome {store.email}!</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className="text-center mt-5">
+        <h1>Welcome !</h1>
+        <div className="display-6">Please sign in or log in</div>
+      </div>
+    );
+  }
 };
